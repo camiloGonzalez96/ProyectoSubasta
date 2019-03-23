c4ee29f5298ecb76105885b84e2d93acc7c976af
camiloGonzalez96 <48777609+camiloGonzalez96@users.noreply.github.com>
2019-03-22 21:18:04 -0500

    interfaz cliente actualizada
    se agrega el segundo panel de productos para mostrar los de la categoria

--- subastaCliente/src/subastacliente/interfazCliente.java
+++ subastaCliente/src/subastacliente/interfazCliente.java
@@ -5,6 +5,8 @@
  */
 package subastacliente;
 
+import java.awt.GridLayout;
+
 /**
  *
  * @author juanc
@@ -17,7 +19,8 @@ public class interfazCliente extends javax.swing.JFrame {
     public interfazCliente() {
         initComponents();
         
-        //ghghghghghghgh
+        GridLayout gridProductos = new GridLayout(2, 3, 5, 5);
+        jPanelProductos.setLayout(gridProductos);
         
         
     }
@@ -35,10 +38,12 @@ public class interfazCliente extends javax.swing.JFrame {
         jPanelPujas = new javax.swing.JPanel();
         jPanelReloj = new javax.swing.JPanel();
         jTextFieldReloj = new javax.swing.JTextField();
+        jLabel1 = new javax.swing.JLabel();
         jPanelInfoProducto = new javax.swing.JPanel();
-        jPanelSalida = new javax.swing.JPanel();
-        jTextFieldSalida = new javax.swing.JTextField();
+        jPanelProductos = new javax.swing.JPanel();
         jTextFieldEntrada = new javax.swing.JTextField();
+        jScrollPane1 = new javax.swing.JScrollPane();
+        jTextAreaCategorias = new javax.swing.JTextArea();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
         getContentPane().setLayout(new org.netbeans.lib.awtextra.AbsoluteLayout());
@@ -52,83 +57,103 @@ public class interfazCliente extends javax.swing.JFrame {
         jPanelPujas.setLayout(jPanelPujasLayout);
         jPanelPujasLayout.setHorizontalGroup(
             jPanelPujasLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 257, Short.MAX_VALUE)
+            .addGap(0, 260, Short.MAX_VALUE)
         );
         jPanelPujasLayout.setVerticalGroup(
             jPanelPujasLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGap(0, 570, Short.MAX_VALUE)
         );
 
-        jPanelPrincipal.add(jPanelPujas, new org.netbeans.lib.awtextra.AbsoluteConstraints(743, 130, -1, 570));
+        jPanelPrincipal.add(jPanelPujas, new org.netbeans.lib.awtextra.AbsoluteConstraints(740, 130, 260, 570));
 
         jPanelReloj.setBackground(new java.awt.Color(33, 97, 140));
 
-        jTextFieldReloj.setText("jTextField1");
+        jTextFieldReloj.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jTextFieldRelojActionPerformed(evt);
+            }
+        });
+
+        jLabel1.setFont(new java.awt.Font("Tahoma", 1, 16)); // NOI18N
+        jLabel1.setForeground(new java.awt.Color(255, 255, 255));
+        jLabel1.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
+        jLabel1.setText("La subasta cierra en:");
 
         javax.swing.GroupLayout jPanelRelojLayout = new javax.swing.GroupLayout(jPanelReloj);
         jPanelReloj.setLayout(jPanelRelojLayout);
         jPanelRelojLayout.setHorizontalGroup(
             jPanelRelojLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(jPanelRelojLayout.createSequentialGroup()
-                .addGap(38, 38, 38)
+            .addComponent(jLabel1, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, jPanelRelojLayout.createSequentialGroup()
+                .addContainerGap(40, Short.MAX_VALUE)
                 .addComponent(jTextFieldReloj, javax.swing.GroupLayout.PREFERRED_SIZE, 185, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addContainerGap(37, Short.MAX_VALUE))
+                .addGap(35, 35, 35))
         );
         jPanelRelojLayout.setVerticalGroup(
             jPanelRelojLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(jPanelRelojLayout.createSequentialGroup()
-                .addGap(27, 27, 27)
-                .addComponent(jTextFieldReloj, javax.swing.GroupLayout.PREFERRED_SIZE, 76, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addContainerGap(27, Short.MAX_VALUE))
+            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, jPanelRelojLayout.createSequentialGroup()
+                .addContainerGap()
+                .addComponent(jLabel1)
+                .addGap(18, 18, 18)
+                .addComponent(jTextFieldReloj, javax.swing.GroupLayout.PREFERRED_SIZE, 47, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addContainerGap(29, Short.MAX_VALUE))
         );
 
         jPanelPrincipal.add(jPanelReloj, new org.netbeans.lib.awtextra.AbsoluteConstraints(740, 0, 260, 130));
 
         jPanelInfoProducto.setBackground(new java.awt.Color(214, 234, 248));
 
+        jPanelProductos.setBackground(new java.awt.Color(46, 134, 193));
+
+        javax.swing.GroupLayout jPanelProductosLayout = new javax.swing.GroupLayout(jPanelProductos);
+        jPanelProductos.setLayout(jPanelProductosLayout);
+        jPanelProductosLayout.setHorizontalGroup(
+            jPanelProductosLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGap(0, 740, Short.MAX_VALUE)
+        );
+        jPanelProductosLayout.setVerticalGroup(
+            jPanelProductosLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGap(0, 390, Short.MAX_VALUE)
+        );
+
         javax.swing.GroupLayout jPanelInfoProductoLayout = new javax.swing.GroupLayout(jPanelInfoProducto);
         jPanelInfoProducto.setLayout(jPanelInfoProductoLayout);
         jPanelInfoProductoLayout.setHorizontalGroup(
             jPanelInfoProductoLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 740, Short.MAX_VALUE)
+            .addComponent(jPanelProductos, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
         );
         jPanelInfoProductoLayout.setVerticalGroup(
             jPanelInfoProductoLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 390, Short.MAX_VALUE)
+            .addComponent(jPanelProductos, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
         );
 
         jPanelPrincipal.add(jPanelInfoProducto, new org.netbeans.lib.awtextra.AbsoluteConstraints(0, 0, 740, 390));
 
-        jPanelSalida.setBackground(new java.awt.Color(93, 173, 226));
-
-        jTextFieldSalida.setText("jTextField1");
-
-        javax.swing.GroupLayout jPanelSalidaLayout = new javax.swing.GroupLayout(jPanelSalida);
-        jPanelSalida.setLayout(jPanelSalidaLayout);
-        jPanelSalidaLayout.setHorizontalGroup(
-            jPanelSalidaLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, jPanelSalidaLayout.createSequentialGroup()
-                .addContainerGap()
-                .addComponent(jTextFieldSalida, javax.swing.GroupLayout.DEFAULT_SIZE, 710, Short.MAX_VALUE)
-                .addContainerGap())
-        );
-        jPanelSalidaLayout.setVerticalGroup(
-            jPanelSalidaLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, jPanelSalidaLayout.createSequentialGroup()
-                .addContainerGap()
-                .addComponent(jTextFieldSalida, javax.swing.GroupLayout.DEFAULT_SIZE, 218, Short.MAX_VALUE)
-                .addContainerGap())
-        );
-
-        jPanelPrincipal.add(jPanelSalida, new org.netbeans.lib.awtextra.AbsoluteConstraints(0, 390, 740, 250));
-
-        jTextFieldEntrada.setText("jTextField2");
+        jTextFieldEntrada.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jTextFieldEntradaActionPerformed(evt);
+            }
+        });
         jPanelPrincipal.add(jTextFieldEntrada, new org.netbeans.lib.awtextra.AbsoluteConstraints(0, 640, 740, 60));
+
+        jTextAreaCategorias.setColumns(20);
+        jTextAreaCategorias.setRows(5);
+        jScrollPane1.setViewportView(jTextAreaCategorias);
+
+        jPanelPrincipal.add(jScrollPane1, new org.netbeans.lib.awtextra.AbsoluteConstraints(0, 390, 740, 250));
 
         getContentPane().add(jPanelPrincipal, new org.netbeans.lib.awtextra.AbsoluteConstraints(0, 0, 1000, 700));
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
+
+    private void jTextFieldRelojActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jTextFieldRelojActionPerformed
+        // TODO add your handling code here:
+    }//GEN-LAST:event_jTextFieldRelojActionPerformed
+
+    private void jTextFieldEntradaActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jTextFieldEntradaActionPerformed
+        // TODO add your handling code here:
+    }//GEN-LAST:event_jTextFieldEntradaActionPerformed
 
     /**
      * @param args the command line arguments
@@ -166,13 +191,15 @@ public class interfazCliente extends javax.swing.JFrame {
     }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JLabel jLabel1;
     private javax.swing.JPanel jPanelInfoProducto;
     private javax.swing.JPanel jPanelPrincipal;
+    private javax.swing.JPanel jPanelProductos;
     private javax.swing.JPanel jPanelPujas;
     private javax.swing.JPanel jPanelReloj;
-    private javax.swing.JPanel jPanelSalida;
+    private javax.swing.JScrollPane jScrollPane1;
+    private javax.swing.JTextArea jTextAreaCategorias;
     private javax.swing.JTextField jTextFieldEntrada;
     private javax.swing.JTextField jTextFieldReloj;
-    private javax.swing.JTextField jTextFieldSalida;
     // End of variables declaration//GEN-END:variables
 }
